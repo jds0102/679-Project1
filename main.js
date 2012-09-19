2a14870513ceaa2eaf07f876709b68a7c13bf476
Michael <michael.masanek@gmail.com>
2012-09-18 22:50:40 -0500

    Cleaned up inheritance

--- main.js
+++ main.js
@@ -16,35 +16,37 @@ SETUP CONSTANTS
 
 	var theCanvas = document.getElementById("mycanvas");
 	var theContext = theCanvas.getContext("2d");
-
-   var playerBall = new ball(300,300,10);
-   var enemy1 = new enemy(300, 100, 10);
-   var enemy2 = new enemy(100, 600, 10);
+    var testBall = new Ball(300,220,-1,-2,5,"#FF00FF");
+    var player = new PlayerBall(450,220,20,20,10,"#00FF00");
+    var enemy1 = new EnemyBall(100,200,5,5,5,"#FF0000");
+    enemy1.follow(player);
+   /*var enemy2 = new enemy(100, 600, 10);
    var enemy3 = new enemy(300, 500, 10);
    var enemy4 = new enemy(400, 400, 10);
    enemy1.follow(playerBall);
    enemy2.follow(enemy1);
    enemy3.follow(enemy1);
-   enemy4.follow(enemy1);
+   enemy4.follow(enemy1);*/
     // this function will do the drawing
     function drawObjects() {
         // clear the window
         theContext.clearRect(0, 0, theCanvas.width, theCanvas.height);
         // draw the balls - too bad we can't use for i in theBalls
-        //anObject.draw();
-		playerBall.draw();
 		enemy1.draw();
-		enemy2.draw();
-		enemy3.draw();
-		enemy4.draw();
+        player.draw()
+		testBall.draw();
     }
-    
+    function updateObjects()
+	{
+	   enemy1.move();
+	   testBall.move();
+	}
     // what to do when things get clicked
     function keyPressed(evt){
         // a catch - we need to adjust for where the canvas is!
         // this is quite ugly without some degree of support from
         // a library
-		playerBall.move(evt.keyCode);
+		player.move(evt.keyCode);
 		
     }
 	
@@ -55,11 +57,12 @@ SETUP CONSTANTS
     // draws, then schedules another iteration in the future
     // WARNING: this is the simplest, but not the best, way to do this
     function drawLoop() {
-		enemy1.move();
+		/*enemy1.move();
 		enemy2.move();
 		enemy3.move();
-		enemy4.move();
-        drawObjects();     // show things
+		enemy4.move();*/
+		updateObjects();
+        drawObjects();
         reqFrame(drawLoop);
     }
     drawLoop();
